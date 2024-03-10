eff13048cfa8274d1f2e0579eb0f6fda9abd67a4
Arturo Avila <mysticmetromx@gmail.com>
2024-03-10 13:50:33 -0600

    Changed 'Full Stack Developer' for 'Software Developer'

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Arturo Avila | Full Stack Developer';
+export const SITE_TITLE = 'Arturo Avila | Software Developer';
 export const SITE_DESCRIPTION = 'A simple portfolio for a simple man.';
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
